82c0fcf77b462b02a5d753ff47c48028fe86d502
kalinydimitrov <kalin.ydimitrov@outlook.com>
2025-09-01 13:38:02 +0300

    Github action commit 2

--- terraform.tf
+++ terraform.tf
@@ -1,20 +1,20 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       # version = "~>5.0"
       version = "~> 6.0"
     }
   }
 
-#   backend "s3" {
-#     bucket = "globo-terraform-state"
-#     key    = "application_config/terraform.tfstate"
-#     region = "us-east-1"
-#     # dynamodb_table = "globo-terraform-locks"
-#     use_lockfile = true
-#     encrypt      = true
+  #   backend "s3" {
+  #     bucket = "globo-terraform-state"
+  #     key    = "application_config/terraform.tfstate"
+  #     region = "us-east-1"
+  #     # dynamodb_table = "globo-terraform-locks"
+  #     use_lockfile = true
+  #     encrypt      = true
 
-#   }
+  #   }
 
 }
