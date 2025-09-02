ed77b5349a7278a778f2b52a544ee2705c7ced6f
kalinydimitrov <kalin.ydimitrov@outlook.com>
2025-09-02 11:36:12 +0300

    app-blabla 5

--- keys.tf
+++ keys.tf
@@ -1,4 +1,4 @@
-# Create SSH Key pair for aws instances using a module
+# Create SSH Key pair for aws instances using a module_
 module "ssh_keys" {
   source  = "terraform-aws-modules/key-pair/aws"
   version = "~>2.0.0"
